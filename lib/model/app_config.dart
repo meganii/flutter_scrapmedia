68ae1e4fdbd332730eee6a906acc5365ee5fc747
meganii <yuhei24@gmail.com>
2020-02-15 17:36:24 +0900

    Extract enum

--- lib/model/app_config.dart
+++ lib/model/app_config.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_scrapmedia/model/config_key.dart';
+import 'package:flutter_scrapmedia/model/scrapmedia_services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AppConfigModel extends ChangeNotifier {
@@ -14,6 +16,12 @@ class AppConfigModel extends ChangeNotifier {
     final storage = new FlutterSecureStorage();
     var value = await storage.readAll();
     _appConfig.addAll(value);
+
+    // Default search method
+    if (_appConfig[ConfigKey.appSearchMethod] == null) {
+      _appConfig[ConfigKey.appSearchMethod.toString()] =
+          ScrapmediaServices.openBDAPI.toString();
+    }
   }
 
   void update(String key, String value) {
